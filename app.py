da26bd63666cb8f3f2be27db0cf7f868b8cac1ca
dafedin <tm.i.sm.master@gmail.com>
2023-08-19 18:20:31 +0300

    Comments added for usability

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ app = Flask(__name__)
 
 
 # Get Holle World route http://localhost:48000/hello
-@app.route('/hello')
+@app.route("/hello")
 def hello_route():  # put application's code here
-    return 'Hello world'
+    return "Hello world"
 
 
 # Get route for named user http://localhost:48000/user/Bob/45
-@app.route('/user/<username>/<int:number>')
+@app.route("/user/<username>/<int:number>")
 def dynamic_route(username, number):
-    return f'Hello {escape(username)}, your number is {number}.'
+    return f"Hello {escape(username)}, your number is {number}."
 
 
 # Get route with arguments as http://localhost:48000/greeting?name=Bob&age=2
-@app.route('/greeting')
+@app.route("/greeting")
 def query_route():
-    name_param = request.args.get('name')
-    age_param = request.args.get('age')
+    name_param = request.args.get("name")
+    age_param = request.args.get("age")
 
-    return f'Hello {name_param}, your number is {age_param}.'
+    return f"Hello {name_param}, your number is {age_param}."
 
 
 # Get route with arguments as http://localhost:48000/webarg_greeting/45?name=Bob
@@ -37,5 +37,5 @@ def user_detail(args, age):
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
